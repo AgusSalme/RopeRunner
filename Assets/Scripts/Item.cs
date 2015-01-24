64ac0ecfd6b011532ef26ab537260060f888e733
Fernando Rodriguez Caton <weemanar@gmail.com>
2015-01-24 16:16:10 -0300

    Agregado deteccion de choques

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,16 +3,29 @@ using System.Collections;
 
 public class Item : MonoBehaviour {
 
-	public float velocidadDespzamiento = 1f;
-
-
+	public float velocidadDespzamiento = 0.1f;
+	public string[] collitionTags = {"Player"};
+	public Camera loseCamera;
+	public Camera inGameCamera;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
-	
 	// Update is called once per frame
 	void Update () {
 		this.transform.Translate(Vector3.back * velocidadDespzamiento);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (inGameCamera.enabled){
+			foreach(string collitionTag in collitionTags){
+				if(other.gameObject.tag == collitionTag){
+					loseCamera.enabled = true;
+					inGameCamera.enabled = false;
+					break;
+				}
+			}
+		}
 	}
 }
