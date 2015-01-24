72f70d4ced781c41405e7546d6a64563d7e8f958
Fernando Rodriguez Caton <weemanar@gmail.com>
2015-01-24 15:20:39 -0300

    Arreglado generador de objetos

--- Assets/Scripts/GeneradorObstaculos.cs
+++ Assets/Scripts/GeneradorObstaculos.cs
@@ -6,6 +6,10 @@ public class GeneradorObstaculos : MonoBehaviour {
 	public GameObject[] obj;
 	public float tiempoMin = 1.25f;
 	public float tiempoMax = 2.5f;
+	public float xMin = -24;
+	public float xMax = 24;
+
+	private float timeForNextGeneration = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +18,15 @@ public class GeneradorObstaculos : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-		Instantiate(obj[Random.Range(0,obj.Length)], transform.position, Quaternion.identity);
-		Invoke("Generar", Random.Range(tiempoMin, tiempoMax));
-}
+		timeForNextGeneration -= Time.deltaTime;
+		if (timeForNextGeneration <= 0) {
+			Vector3 position = transform.position;
+			position.x = Random.Range (xMin, xMax);
+			Instantiate (obj [Random.Range (0, obj.Length)], position, Quaternion.identity);
+			timeForNextGeneration = Random.Range(tiempoMin, tiempoMax);
+		}
+		//Invoke("Generar", Random.Range(tiempoMin, tiempoMax));
+
+	}
 
 }
