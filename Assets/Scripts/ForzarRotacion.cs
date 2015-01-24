a6c7d6a6d065af4654d74e0dc3fcd032c8751c1d
agustin <agustin.s_a@hotmail.com>
2015-01-24 18:32:45 -0300

    "Generadores de arbustos y cambios en Game"

---
+++ Assets/Scripts/ForzarRotacion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForzarRotacion : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+		transform.Rotate (new Vector3(90,0,0));
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
