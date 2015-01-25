f5fd3e85c80a041a098fc56b39d7b6cae4bf8db0
Santiago <santiracca3@gmail.com>
2015-01-25 15:54:29 -0300

    intro cambiada

---
+++ Assets/Scripts/MovieScriptDuck.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MovieScriptDuck : MonoBehaviour {
+		public GameObject otherPlane;
+		// Use this for initialization
+		void Start()
+		{
+			((MovieTexture)renderer.material.mainTexture).Play ();
+			Invoke("PlaySound", 1);
+			Invoke("ChangePlane", 5);
+		}
+
+		void PlaySound(){
+			audio.Play();
+		}
+
+		void ChangePlane(){
+			Destroy (this.gameObject);
+			otherPlane.SetActive (true);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+
+			
+		}
+
+}
