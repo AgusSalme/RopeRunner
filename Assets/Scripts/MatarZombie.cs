c52e2509dc5de9420c4baf6b502b6102b2e46846
agustin <agustin.s_a@hotmail.com>
2015-01-25 14:37:36 -0300

    Script zombiemuerte

---
+++ Assets/Scripts/MatarZombie.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MatarZombie : MonoBehaviour {
+
+	Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Rope") {
+			anim.SetBool("isDead", true);
+			audio.Play();
+		} 
+	}
+
+}
+
+
