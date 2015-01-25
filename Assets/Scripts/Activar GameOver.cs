fd1090da2c5ccc3997dbcbd8284315b405fccebe
agustin <agustin.s_a@hotmail.com>
2015-01-25 10:56:32 -0300

    Beta V5 Programacion Score y Actualizacion GameOver en Progreso

---
+++ Assets/Scripts/Activar GameOver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivarGameOver : MonoBehaviour {
+
+	public GameObject CamaraGameOver;
+
+	// Use this for initialization
+	void Start () {
+		NotificationCenter.DefaultCenter().AddObserver(this, "PersonajeHaMuerto");
+	}
+
+	void PersonajeHaMuerto(Notification notification){
+		CamaraGameOver.SetActive(true);
+	  }
+		
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,27 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class Item : MonoBehaviour {
 
 	public float velocidadDespzamiento = 0.1f;
-	public string[] collitionTags = {"Player"};
-	public string loseSceneName = "Lose";
+	public bool itemgenerado = false;
+	Animator anim;
+	
+	void Awake () {
+		anim = GetComponent<Animator> ();
+		}
 
-	// Use this for initialization
-	void Start () {
-		
+	void FixedUpdate () {
+				if (itemgenerado)
+						this.transform.Translate (Vector3.down * velocidadDespzamiento);
+				} else {
+		itemgenerado = true;
+		NotificationCenter.DefaultCenter ().PostNotification (this, "ItemSeEstanGenerando");
 	}
+
 	// Update is called once per frame
 	void Update () {
-		this.transform.Translate(Vector3.down * velocidadDespzamiento);
+		
 	}
 
 	void OnTriggerEnter(Collider other) {
-		foreach(string collitionTag in collitionTags){
-			if(other.gameObject.tag == collitionTag){
-				Application.LoadLevel(loseSceneName);
-				break;
-			}
+				if (other.tag == "Player") {
+						NotificationCenter.DefaultCenter ().PostNotification (this, "PersonajeHaMuerto");
+						GameObject Player = GameObject.Find ("Player");
+						Player.SetActive (false);
+				} else {
+						Destroy (other.gameObject);
+				}
 		}
-	}
 }
